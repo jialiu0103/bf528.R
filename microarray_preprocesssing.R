04d0c9eb54efd925c6b7e473f8b6211374c2923f
Jia Liu <60714219+jialiu0103@users.noreply.github.com>
2020-06-29 13:16:37 -0400

    Update and rename qc.R to microarray_preprocesssing.R
    readin array, quality control, normalize

--- microarray_preprocesssing.R
+++ microarray_preprocesssing.R
@@ -1,9 +1,4 @@
----
-title: "stemaway_microarray"
-output: html_document
----
 
-```{install}
 if (!requireNamespace("BiocManager", quietly = TRUE))
     install.packages("BiocManager")
 
@@ -12,28 +7,25 @@ BiocManager::install("affyPLM")
 BiocManager::install("sva")
 BiocManager::install("AnnotationDbi")
 BiocManager::install("hgu133plus2.db")
-```
 
-```{library}
 library("affy")
 library("affyPLM")
 library("sva")
 library("AnnotationDbi")
 library("hgu133plus2.db")
-```
+
 ##install and libaray packages 
 
 
-```{readin dataset}
+
 normal_cels='/Users/liujia/Desktop/intern/datasets/NnC'
 affy_norm = ReadAffy(celfile.path=normal_cels)
 View(affy_norm)
 norm_arry <- rma(affy_norm)
 getClass(affy_norm)
-```
 ##read in array and normalize 
 
-```{Relative Log Expression, Normalized Unscaled Standard Error}
+##Relative Log Expression, Normalized Unscaled Standard Error
 ##compute RLE and NUSE scores of the microarray samples
 my_plm<- fitPLM(affy_norm,normalize = T, background = T)
 Mbox(my_plm,main="RLE") #this is a boxplot of rle. cause i want to take a look at distribution
@@ -42,16 +34,10 @@ sta_rle=RLE(my_plm,type="stats")
 my_med=sta_rle[1,]
 #plot histgram
 hist_rle=hist(my_med)
-```
-```{nuse histgram}
+
+##nuse histgram
 sta_nuse=NUSE(my_plm,type="stats")
 my_med_nuse=sta_nuse[1,]
 hist_nuse=hist(my_med_nuse)
-```
-
-##use limma to analyse gene expression differences
-```{limma}
-library(limma)
 
 
-```
