c28aada5c5c19464840fe7c7bbb6ac8496d7afcb
Jia Liu <60714219+jialiu0103@users.noreply.github.com>
2020-07-29 13:33:15 -0400

    Update and rename July_prepro&visualize.R to July_session.R

--- July_session.R
+++ July_session.R
@@ -91,4 +91,81 @@ pheatmap(dismat, annotation_col = annotation_col,show_rownames=F,main = 'hierarc
 
 
 
+#DE analysie
+##################ANNOTATION
+library('hgu133plus2.db')
+x <- hgu133plus2.db
+keys(x)
+columns(x)
+keytypes(x)
+
+# Map Probe IDs to Gene Symbols
+cols <- c("SYMBOL", "GENENAME")
+Sym_ID2 <- AnnotationDbi::select(x, keys=as.character(rownames(combat_norm_matrix)), columns='SYMBOL')
+table=na.omit(Sym_ID2)
+
+# make a dataframe contains gene expression, probeid and annotation
+ind <- match(rownames(combat_norm_matrix), table$PROBEID)
+probeid=rownames(combat_norm_matrix)
+ge <- as.data.frame(combat_norm_matrix)
+ge$probeid=probeid
+ge$Gene_Symbol <- table$SYMBOL[ind]
+
+# drop duplicated rows
+annot_gene<-ge[!duplicated(ge$Gene_Symbol), ] 
+#annot_gene<-ge[unique(ge$Gene_Symbol), ] #you can also use unique() to drop duplication
+
+
+#######filtering lowest 4% genes
+aa=annot_gene[,1:98]
+annot_gene$gesum=rowSums(aa)
+#library(genefilter)
+lowthre=quantile(annot_gene$gesum,0.04)
+filt_gene=annot_gene[which(annot_gene$gesum>lowthre),]
+filt_gene=na.omit(filt_gene)
+filt_matrix=filt_gene[,1:98]
+rownames(filt_matrix)<-filt_gene$Gene_Symbol
+#rownames(filt_matrix)<-rownames(filt_gene)
+#######use limma to do filtering
+library(limma)
+group <- factor(meta$CN,levels = c("Normal","Cancer"),ordered = F)
+design <- model.matrix(~group)
+colnames(design) <- levels(mygroup)
+rownames(design) <- colnames(filt_matrix)
+fit <- lmFit(filt_matrix, design)
+fit <- eBayes(fit)
+output <- topTable(fit, coef=2,n=Inf)
+#sum(output$adj.P.Val<0.05)
+sigout=output[output$adj.P.Val<0.05,]
+# I specify coef=2 because we are interested in the difference between groups, not the intercept.
+
+############################## volcano plot
+library(EnhancedVolcano)
+## Sort by ordered adj.P.Val
+res_tableOE_ordered <- output[order(abs(output$logFC),decreasing = TRUE), ] 
+## Create a column to indicate which genes to label
+res_tableOE_ordered$genelabels <- ""
+res_tableOE_ordered$genelabels[1:50] <- rownames(res_tableOE_ordered)[1:50]
+#View(res_tableOE_ordered)
+EnhancedVolcano(res_tableOE_ordered,
+                'logFC', y = 'adj.P.Val',
+                lab = res_tableOE_ordered$genelabels)
+
+#heatmap for significant genes
+hmdata=sigout[abs(sigout$logFC)>2.8,]
+library(pheatmap)
+ind <- match(rownames(hmdata), rownames(filt_matrix))
+myre= filt_matrix[ind,]
+nm=meta$CN
+annotation_col = data.frame(SampleType = nm)
+rownames(annotation_col) = sample_lab
+colnames(myre) = sample_lab
+pheatmap(myre, annotation_col = annotation_col,main = 'top 50 significant DEGs')
+
+#another way to do heatmap
+top.genes <- topTable(fit, coef=2, p.value = 0.05, number=50)
+heatmap_InputMatrix<-filt_matrix[(rownames(filt_matrix) %in%  rownames(top.genes)),]
+colnames(heatmap_InputMatrix)=sample_lab
+pheatmap(heatmap_InputMatrix, annotation_col = annotation_col,main = 'top 50 significant DEGs')
+
 
